0a411367887162ae1c38e5d6fb180e9a6f3d35bf
Shruti Baviskar <shrutibaviskar224@gmail.com>
2025-03-10 14:37:15 +0530

    Create identify operator.py

---
+++ identify operator.py
@@ -0,0 +1,12 @@
+x=['a','b','c']
+y=['t','r','i']
+w=['a','b','c']
+z=x
+print(x is y)
+print(x is not y)
+print(x is z)
+print(x is not z)
+print(x==y)
+print(x!=y)
+print(x is w)
+print(x is not w)
